04a5481821dcc5c16516b51b6b950ea03eefe66e
Mike Ulvila <mikeulvila@me.com>
2015-10-13 12:12:04 -0500

    finished grades exercise

--- grades/script.js
+++ grades/script.js
@@ -22,8 +22,61 @@ for (var i = 0; i < scores.length; i++) {
 		letterGrade.push("A");
 	}
 };
-
 console.log(letterGrade);
 
 // SEPERATE LETTER GRADES
-for (var i = 0; i < letterGrade.length)
+for (var i = 0; i < letterGrade.length; i++) {
+	if (letterGrade[i] === "A") {
+		gradeA.push(letterGrade[i]);
+	} else if (letterGrade[i] === "B") {
+		gradeB.push(letterGrade[i]);
+	} else if (letterGrade[i] === "C") {
+		gradeC.push(letterGrade[i]);
+	} else if (letterGrade[i] === "D") {
+		gradeD.push(letterGrade[i]);
+	} else if (letterGrade[i] === "F") {
+		gradeF.push(letterGrade[i]);
+	}
+}
+
+console.log("A's = ", gradeA.length);
+console.log("B's = ", gradeB.length);
+console.log("C's = ", gradeC.length);
+console.log("D's = ", gradeD.length);
+console.log("F's = ", gradeF.length);
+
+// FIND LOW GRADE
+var lowGrade = 100;
+var currentGrade;
+for (var i = 0; i < scores.length; i++) {
+	currentGrade = scores[i];
+	if (currentGrade < lowGrade) {
+		lowGrade = currentGrade;
+	} else {
+		lowGrade = lowGrade;
+	}
+}
+console.log("Low Grade", lowGrade);
+
+// FIND HIGH GRADE
+var highGrade = 0;
+
+for (var i = 0; i < scores.length; i++) {
+	currentGrade = scores[i];
+	if (currentGrade > highGrade) {
+		highGrade = currentGrade;
+	} else {
+		highGrade = highGrade;
+	}
+}
+
+console.log("High Grade", highGrade);
+
+
+
+
+
+
+
+
+

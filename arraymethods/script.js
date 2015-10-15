a62abf1b139b9631f6fca578f1e55702edae0462
Mike Ulvila <mikeulvila@me.com>
2015-10-15 14:29:50 -0500

    changed insertion to div

--- arraymethods/script.js
+++ arraymethods/script.js
@@ -3,11 +3,11 @@ console.log("test");
 var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
 
 // Use the forEach method to add the name of each planet to a div element in your HTML
-var outputPlanet = "";
+// var outputPlanet = "";
 planets.forEach(function (planet) {
-	outputPlanet += "<p>" + planet + "</p>";
+	document.getElementById("planet-names").innerHTML += "<p>" + planet + "</p>";
 });
-document.getElementById("planet-names").innerHTML = outputPlanet;
+// document.getElementById("planet-names").innerHTML = outputPlanet;
 
 // Use the map method to create a new array where the first letter of each planet is capitalized
 var capsPlanets = planets.map(function (planet) {
@@ -27,3 +27,46 @@ var sentence = words.reduce(function (prev, curr) {
 	return prev + " " + curr;
 })
 console.log(sentence);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
